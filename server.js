7fe5ded3123025fc24635dca143fe8ec439e4ed9
jacoblovins <lovins.jacob@yahoo.com>
2020-08-20 19:18:57 -0400

    added more files

--- server.js
+++ server.js
@@ -12,36 +12,11 @@ const PORT = process.env.PORT || 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+
 app.use(express.static(path.join(__dirname, 'public')));
 
-// // Star Wars Characters (DATA)
-// // =============================================================
-// const characters = [
-//   {
-//     routeName: "yoda",
-//     name: "Yoda",
-//     role: "Jedi Master",
-//     age: 900,
-//     forcePoints: 2000
-//   },
-//   {
-//     routeName: "darthmaul",
-//     name: "Darth Maul",
-//     role: "Sith Lord",
-//     age: 200,
-//     forcePoints: 1200
-//   },
-//   {
-//     routeName: "obiwankenobi",
-//     name: "Obi Wan Kenobi",
-//     role: "Jedi Master",
-//     age: 55,
-//     forcePoints: 1350
-//   }
-// ];
 
-// Routes
-// =============================================================
+
 
 // Basic route that sends the user first to the AJAX Page
 app.get("/home.html", (req, res) => {
@@ -80,22 +55,22 @@ app.get("/api/characters/:character", (req, res) => {
   return res.json(false);
 });
 
-// Create New Characters - takes in JSON input
-app.post("/api/characters", (req, res) => {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  const newCharacter = req.body;
+// // Create New Characters - takes in JSON input
+// app.post("/api/reserve", (req, res) => {
+//   // req.body hosts is equal to the JSON post sent from the user
+//   // This works because of our body parsing middleware
+//   const newCharacter = req.body;
 
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+//   // Using a RegEx Pattern to remove spaces from newCharacter
+//   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+//   newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
 
-  console.log(newCharacter);
+//   console.log(newCharacter);
 
-  characters.push(newCharacter);
+//   characters.push(newCharacter);
 
-  res.json(newCharacter);
-});
+//   res.json(newCharacter);
+// });
 
 // Starts the server to begin listening
 // =============================================================
